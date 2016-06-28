4b913623f37781e8daf4bc5c93d8989d03e1019d
Yaqi Shi <yshi39@dons.usfca.edu>
2016-06-28 11:51:04 -0700

    add load_xml.py

---
+++ load_xml.py
@@ -0,0 +1,476 @@
+from py2neo import Graph, Node
+import os
+
+
+g= Graph("http://localhost:7474/browser/",password = "donsusfcaedu")
+g.delete_all()
+tx = g.begin()
+
+
+#========================================== Get files ==========================================#
+root =  os.getcwd()
+path = os.path.join(root, "data")
+disclosure_1st_path = os.path.join(path, "2013_1stQuarter_XML")
+files = [f for f in os.listdir(disclosure_1st_path) if f.endswith('.xml')]           # Return xml files
+for file in files:
+    fi = 'file://' + os.path.join(disclosure_1st_path, file)
+
+
+# ========================================== Node: Disclosure ==========================================#
+    dc_id = ''
+    # dc_id is set to '' at the beginning of each iteration
+    disclosure = g.run(
+        '''CALL apoc.load.xml({f})
+        YIElD value as ld2
+        WITH [attr in ld2._children
+        WHERE attr._type in ['pages','houseID','senateID','reportYear'] | [attr._type, attr._text]] as pairs
+        CALL apoc.map.fromPairs(pairs)
+        YIELD value
+        CREATE (dc:Disclosure {pages: value.pages, senateID: value.senateID,
+        houseID: value.houseID, reportYear: value.reportYear, fileid : {fi}})
+        RETURN id(dc)''',
+        parameters={'f': fi, 'fi':fi[-14:]}
+    )
+    dc_id = [dc['id(dc)'] for dc in disclosure][0]
+    # disclosure is a cursor object, dc ['id(dc)'] return a list.
+
+
+# ========================================== Node: lobby firm ==========================================#
+    # TODO(Yaqi): add code here
+
+
+# ========================================== Node: clients ==========================================#
+    # TODO(Yaqi): add code here
+
+
+#========================================== Node: lobbyist ==========================================#
+    issue_id_lst = []
+    # issue_id_lst is set to [] at the beginning of each iteration
+    lobbyists_issuecode = g.run(
+        '''CALL apoc.load.xml({f})
+        YIElD value UNWIND value._children as dc
+        WITH dc WHERE dc._type = 'alis'
+        UNWIND dc._children as alis
+        UNWIND alis._children as ali_info
+        WITH ali_info WHERE ali_info._type in ['issueAreaCode','lobbyists']
+        RETURN ali_info''',
+        parameters={'f':fi}
+    )
+    # Cypher returns a cursor object, each item in the cursor object represents a issuecode and lobbyists who are associated to that issuecode
+
+    iscd_lst = []
+    # collect issuecodes of one disclosure
+    # find_empty = False
+    #find_empty data
+    for i,lobbyist_issuecode in enumerate(lobbyists_issuecode):
+
+        lobOrIss_item = lobbyist_issuecode['ali_info']
+        #each item in the cursor object is a JSON object and represents a issuecode and lobbyists who are associated to that issuecode
+        # Both lobbyists and issuecodes in the JSON object have attr '_type';
+        # Lobbyists have attr '_children'
+        # Issuecodes have attr '_text'
+        if not('_text' in lobOrIss_item or '_children' in lobOrIss_item ):
+            # The if statement check if an item doest not have attr '_text' nor '_children', the JSON object has no
+            # issuecode data or lobbyists data to be extracted
+            find_empty = True
+            # here find empty data
+            break
+
+        elif '_text' in lobOrIss_item:
+            # check if '_text' in the item, the item represents an issuecode
+            issuecode = lobOrIss_item['_text']
+            iscd_lst.append(issuecode)
+            # collecting issuecodes to the idscd_lst
+            issues_node = g.run(
+                '''CREATE (iss:Issue {code: {code}})
+                RETURN id(iss)
+                ''', parameters = {'code': issuecode}
+            )
+            issue_id = [issue['id(iss)'] for issue in issues_node][0]
+            issue_id_lst.append(issue_id)
+            # Collect node'id of new (iss: Issue) created at each iteration
+
+        elif '_children' in lobOrIss_item :
+            # check if '_children' in the item, the item represents lobbyists
+            #lobbyists_json = lobOrIss_item
+            lobbyists = lobOrIss_item['_children']
+            # lobbyist_lst = []
+            lob_id_lst = []
+
+            for lobbyist in lobbyists:
+
+                lobbyist_dic = {}
+                first_name = lobbyist['_children'][0]
+                last_name = lobbyist['_children'][1]
+                position = lobbyist['_children'][3]
+
+                if ('_text' in first_name) and ('_text' in last_name):
+
+                    lobbyist_dic['firstName'] = first_name['_text']
+                    lobbyist_dic['lastName'] = last_name['_text']
+
+                    try:
+
+                        lobbyist_dic['position'] = position['_text']
+                        # position is optional in the XML file
+
+                    except KeyError:
+
+                        lobbyist_dic['position'] = "NULL"
+
+                    # lobbyist_lst.append(lobbyist_dic)
+                    lobbyists_node = g.run(
+                        ''' MERGE (lob: Lobbyist {firstName: {firstName}, lastName: {lastName}, position : {position}})
+                        RETURN id(lob)
+                        ''', parameters={'firstName': lobbyist_dic['firstName'],
+                                         'lastName': lobbyist_dic['lastName'],
+                                         'position': lobbyist_dic['position']}
+                    )
+                    lob_id = [lobbyist['id(lob)'] for lobbyist in lobbyists_node][0]
+                    lob_id_lst.append(lob_id)
+
+            # print(issue_id)
+            # print(lobbyist_lst)
+            # print(lob_id_lst)
+            lobbyist_iss_rel = g.run(
+                '''Match (iss:Issue) WHERE id(iss) = {issue_id}
+                MATCH (lob: Lobbyist) WHERE id(lob) in {lob_id_lst}
+                CREATE (lob)-[r:lobbies]->(iss)
+                ''', parameters={'issue_id': issue_id, 'lob_id_lst': lob_id_lst}
+            )
+    # if find_empty:
+    #     break
+# ========================================== Node: Issue ==========================================#
+    issuesdes = g.run(
+        '''CALL apoc.load.xml({f})
+        YIElD value UNWIND value._children as dl
+        WITH dl WHERE dl._type = 'alis'
+        UNWIND dl._children as alis
+        UNWIND alis._children as ali_info
+        WITH ali_info WHERE ali_info._type = 'specific_issues'
+        UNWIND ali_info._children as specific_issues
+        RETURN specific_issues._text''', parameters={'f': fi})
+
+    # desc = [des['specific_issues._text'] for des in issuesdes][0]
+    # print(desc) "None"
+
+
+    federal_agencies = g.run(
+        '''CALL apoc.load.xml({f})
+        YIElD value UNWIND value._children as dl
+        WITH dl WHERE dl._type = 'alis'
+        UNWIND dl._children as alis
+        UNWIND alis._children as ali_info
+        WITH ali_info WHERE ali_info._type = 'federal_agencies'
+        RETURN ali_info._text''', parameters={'f': fi})
+
+    isde_lst = []
+    feag_lst = []
+
+    for issuedes in issuesdes:
+        isde_lst.append(issuedes['specific_issues._text'])
+
+    for agency in federal_agencies:
+        feag_lst.append(agency['ali_info._text'])
+
+    # print(len(iscd_lst))
+    # print(len(isde_lst))
+    # print(len(feag_lst))
+    if len(iscd_lst) == len(isde_lst) and len(isde_lst) == len(feag_lst):
+
+        for i in range(len(iscd_lst)):
+            issues = g.run(
+                ''' MATCH (iss:Issue) WHERE id(iss) in {issue_id_lst}
+                SET iss.des = {des}, iss.agency = {agency}
+                ''', parameters={'issue_id_lst': issue_id_lst, "des" : isde_lst[i], "agency": feag_lst[i]}
+            )
+
+    elif len(iscd_lst) != len(isde_lst) and len(isde_lst) != len(feag_lst):
+        print('error')
+
+
+    # #===================== Rel=====================#
+    dc_iss_rel = g.run(
+        '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
+        MATCH (iss:Issue) WHERE id(iss) in {iss_id}
+        CREATE (dc)-[r:has]->(iss)
+        ''',
+        parameters = { 'dc_id': dc_id, 'iss_id': issue_id_lst}
+    )
+
+
+
+# fi = 'e'
+#
+#
+#     #===================== Node: Disclosure =====================#
+#     disclosure = g.run(
+#         '''CALL apoc.load.xml({f})
+#         YIElD value as ld2
+#         WITH [attr in ld2._children
+#         WHERE attr._type in ['houseID','senateID','reportYear'] | [attr._type, attr._text]] as pairs
+#         CALL apoc.map.fromPairs(pairs)
+#         YIELD value
+#         CREATE (dc:Disclosure {senateID: value.senateID, houseID: value.houseID, reportYear: value.reportYear, fileid: {fi}})
+#         RETURN id(dc)''',
+#         parameters={'f': fi, 'fi':fi[-14:]})
+#
+#     for dc in disclosure:
+#         dc_id = dc['id(dc)']
+
+    #
+    # #===================== Node: Issue =====================#
+    # issuecodes = g.run(
+    #     '''CALL apoc.load.xml({f})
+    #     YIElD value UNWIND value._children as dl
+    #     WITH dl WHERE dl._type = 'alis'
+    #     UNWIND dl._children as alis
+    #     UNWIND alis._children as ali_info
+    #     WITH ali_info WHERE ali_info._type = 'issueAreaCode'
+    #     RETURN ali_info._text''', parameters={'f': fi})
+    #
+    # issuesdes = g.run(
+    #     '''CALL apoc.load.xml({f})
+    #     YIElD value UNWIND value._children as dl
+    #     WITH dl WHERE dl._type = 'alis'
+    #     UNWIND dl._children as alis
+    #     UNWIND alis._children as ali_info
+    #     WITH ali_info WHERE ali_info._type = 'specific_issues'
+    #     UNWIND ali_info._children as specific_issues
+    #     RETURN specific_issues._text''', parameters={'f': fi})
+    #
+    # federal_agencies = g.run(
+    #     '''CALL apoc.load.xml({f})
+    #     YIElD value UNWIND value._children as dl
+    #     WITH dl WHERE dl._type = 'alis'
+    #     UNWIND dl._children as alis
+    #     UNWIND alis._children as ali_info
+    #     WITH ali_info WHERE ali_info._type = 'federal_agencies'
+    #     RETURN ali_info._text''', parameters={'f': fi})
+    #
+    # iscd_lst = []
+    # isde_lst = []
+    # feag_lst = []
+    #
+    #
+    # for issuecode in issuecodes:
+    #     iscd_lst.append(issuecode['ali_info._text'])
+    #
+    # for issuedes in issuesdes:
+    #     # print(issuedes)
+    #     isde_lst.append(issuedes['specific_issues._text'])
+    #
+    # for agency in federal_agencies:
+    #     feag_lst.append(agency['ali_info._text'])
+    #
+    # if len(iscd_lst) == len(isde_lst) and len(isde_lst) == len(feag_lst):
+    #     for i in range(len(iscd_lst)):
+    #         # issue_dict = {}
+    #         # issue_dict['code'] = iscd_lst[i]
+    #         # issue_dict['des'] = isde_lst[i]
+    #         # issue_dict['agency'] = feag_lst[i]
+    #         # issue_collector.append(issue_dict)
+    #         issues = g.run(
+    #             '''CREATE (iss:Issue {code: {code}, des:{des}, agency:{agency}})
+    #             RETURN id(iss)''',
+    #             parameters={"code": iscd_lst[i], "des" : isde_lst[i], "agency": feag_lst[i]})
+    #
+    #         for issue in issues:
+    #             iss_id.append(issue['id(iss)'])
+    #
+    # else:
+    #     print('error')
+    #
+    #
+    # # #===================== Rel=====================#
+    # dc_iss_rel = g.run(
+    #     '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
+    #     MATCH (iss:Issue) WHERE id(iss) in {iss_id}
+    #     CREATE (dc)-[r:has]->(iss)
+    #     ''',
+    #     parameters = { 'dc_id': dc_id, 'iss_id': iss_id}
+    # )
+    # print(iss_id)
+    # print(dc_id)
+
+
+
+
+
+
+
+
+
+
+
+
+# # # #===================== Node: Disclosure =====================## #===================== Node: Disclosure =====================## #===================== Node: Disclosure =====================#
+# #
+# #
+# fi = 'file:///Users/yaqi/Documents/vir_health_graph/health-graph/data/2013_1stQuarter_XML/300529228.xml'
+#
+#
+# #===================== Node: Disclosure =====================#
+# disclosure = g.run(
+#     '''CALL apoc.load.xml({f})
+#     YIElD value as ld2
+#     WITH [attr in ld2._children
+#     WHERE attr._type in ['houseID','senateID','reportYear'] | [attr._type, attr._text]] as pairs
+#     CALL apoc.map.fromPairs(pairs)
+#     YIELD value
+#     CREATE (dc:Disclosure {senateID: value.senateID, houseID: value.houseID, reportYear: value.reportYear})
+#     RETURN id(dc)''',
+#     parameters={'f': fi})
+# dc_id = [dc['id(dc)'] for dc in disclosure][0]
+#
+# #===================== Node: Disclosure =====================#
+# disclosure = g.run(
+#     '''CALL apoc.load.xml({f})
+#     YIElD value as ld2
+#     WITH [attr in ld2._children
+#     WHERE attr._type in ['houseID','senateID','reportYear'] | [attr._type, attr._text]] as pairs
+#     CALL apoc.map.fromPairs(pairs)
+#     YIELD value
+#     CREATE (dc:Disclosure {senateID: value.senateID, houseID: value.houseID, reportYear: value.reportYear})
+#     RETURN id(dc)''',
+#     parameters={'f': fi})
+# dc_id = [dc['id(dc)'] for dc in disclosure][0]
+#
+# #===================== Node: lobbyist =====================#
+# lobbyists_issuecode = g.run(
+#     '''CALL apoc.load.xml({f})
+#     YIElD value UNWIND value._children as dl
+#     WITH dl WHERE dl._type = 'alis'
+#     UNWIND dl._children as alis
+#     UNWIND alis._children as ali_info
+#     WITH ali_info WHERE ali_info._type in ['issueAreaCode','lobbyists']
+#     RETURN ali_info''', parameters={'f':fi})
+#
+# iscd_lst = []
+# issue_id_lst = []
+#
+# for i,lobbyist_issuecode in enumerate(lobbyists_issuecode):
+#
+#     if not '_children' in lobbyist_issuecode['ali_info']:
+#
+#         issuecode1 = lobbyist_issuecode['ali_info']['_text']
+#         iscd_lst.append(issuecode1)
+#         issues = g.run(
+#             '''CREATE (iss:Issue {code: {code}})
+#             RETURN id(iss)
+#             ''', parameters = {'code': issuecode1}
+#         )
+#
+#         issue_id = [issue['id(iss)'] for issue in issues][0]
+#         issue_id_lst.append(issue_id)
+#
+#     else:
+#
+#         lobbyists_json = lobbyist_issuecode['ali_info']
+#         lobbyist_lst = []
+#         lob_id_lst = []
+#
+#         for lobbyist in lobbyists_json['_children']:
+#
+#             lobbyist_dic = {}
+#
+#             if ('_text' in lobbyist['_children'][0]) and ('_text' in lobbyist['_children'][1]):
+#
+#                 lobbyist_dic['firstName'] = lobbyist['_children'][0]['_text']
+#                 lobbyist_dic['lastName'] = lobbyist['_children'][1]['_text']
+#
+#                 try:
+#
+#                     lobbyist_dic['position'] = lobbyist['_children'][3]['_text']
+#
+#                 except KeyError:
+#
+#                     lobbyist_dic['position'] = "NULL"
+#
+#                 lobbyist_lst.append(lobbyist_dic)
+#                 lobbyists = g.run(
+#                     ''' MERGE (lob: Lobbyist {firstName: {firstName}, lastName: {lastName}, position : {position}})
+#                     RETURN id(lob)
+#                     ''', parameters={'firstName': lobbyist_dic['firstName'],
+#                                      'lastName': lobbyist_dic['lastName'],
+#                                      'position': lobbyist_dic['position']}
+#                 )
+#                 lob_id = [lobbyist['id(lob)'] for lobbyist in lobbyists][0]
+#                 lob_id_lst.append(lob_id)
+#
+#         # print(issue_id)
+#         # print(lobbyist_lst)
+#         # print(lob_id_lst)
+#         lobbyist_iss_rel = g.run(
+#             '''Match (iss:Issue) WHERE id(iss) = {issue_id}
+#             MATCH (lob: Lobbyist) WHERE id(lob) in {lob_id_lst}
+#             CREATE (lob)-[r:lobbies]->(iss)
+#             ''', parameters={'issue_id': issue_id, 'lob_id_lst': lob_id_lst}
+#         )
+#
+# # ===================== Node: Issue =====================#
+# issuesdes = g.run(
+#     '''CALL apoc.load.xml({f})
+#     YIElD value UNWIND value._children as dl
+#     WITH dl WHERE dl._type = 'alis'
+#     UNWIND dl._children as alis
+#     UNWIND alis._children as ali_info
+#     WITH ali_info WHERE ali_info._type = 'specific_issues'
+#     UNWIND ali_info._children as specific_issues
+#     RETURN specific_issues._text''', parameters={'f': fi})
+#
+# federal_agencies = g.run(
+#     '''CALL apoc.load.xml({f})
+#     YIElD value UNWIND value._children as dl
+#     WITH dl WHERE dl._type = 'alis'
+#     UNWIND dl._children as alis
+#     UNWIND alis._children as ali_info
+#     WITH ali_info WHERE ali_info._type = 'federal_agencies'
+#     RETURN ali_info._text''', parameters={'f': fi})
+#
+# isde_lst = []
+# feag_lst = []
+#
+# for issuedes in issuesdes:
+#     isde_lst.append(issuedes['specific_issues._text'])
+#
+# for agency in federal_agencies:
+#     feag_lst.append(agency['ali_info._text'])
+#
+# if len(iscd_lst) == len(isde_lst) and len(isde_lst) == len(feag_lst):
+#
+#     for i in range(len(iscd_lst)):
+#         issues = g.run(
+#             ''' MATCH (iss:Issue) WHERE id(iss) in {issue_id_lst}
+#             SET iss.des = {des}, iss.agency = {agency}
+#             ''', parameters={'issue_id_lst': issue_id_lst, "des" : isde_lst[i], "agency": feag_lst[i]}
+#         )
+#
+# else:
+#     print('error')
+#
+#
+# # #===================== Rel=====================#
+# dc_iss_rel = g.run(
+#     '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
+#     MATCH (iss:Issue) WHERE id(iss) in {iss_id}
+#     CREATE (dc)-[r:has]->(iss)
+#     ''',
+#     parameters = { 'dc_id': dc_id, 'iss_id': issue_id_lst}
+# )
+
+#
+#
+#
+# # tx.create(issue)
+# # tx.run(disclosure)
+# # tx.run(lobbyfirm)
+# # tx.run(client)
+# # tx.commit()
+# #
+# # data = open(file, 'r')
+# # stng = data.read()
+# # print(stng)
+# # x = ET.fromstring(stng)
+# # print(x)
+
