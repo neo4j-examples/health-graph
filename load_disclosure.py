b22c861b994c2f24e1232988467b6178befb147d
Yaqi Shi <yshi39@dons.usfca.edu>
2016-07-06 13:01:53 -0700

    merge lobbyist while creating lob-iss-rel

--- load_disclosure.py
+++ load_disclosure.py
@@ -3,6 +3,7 @@ import os
 
 # ========================================== Node: Disclosure ==========================================#
 
+
 def get_Disclosure_property (file):
     '''
     :param file: the xml file path to be parsed
@@ -20,6 +21,7 @@ def get_Disclosure_property (file):
 
     return g.run(query, file = file).evaluate()
 
+
 def create_Disclousure_node (properties, file):
     '''
     :param properties: the properties of the node
@@ -31,7 +33,7 @@ def create_Disclousure_node (properties, file):
     RETURN id(dc)
     '''
 
-    return g.run(query, properties = properties, file = file)
+    return g.run(query, properties = properties, file = file).evaluate()
 
 
 # ========================================== Node: lobby firm ==========================================#
@@ -122,10 +124,12 @@ def create_LobbyFirm_node(properties):
         RETURN id(lf)
         '''
 
-    return g.run(query, houseOrgId = properties['houseOrgId'], properties=properties)
+    return g.run(query, houseOrgId = properties['houseOrgId'], properties=properties).evaluate()
 
 
 # # ========================================== Node: clients ==========================================#
+
+
 def get_Client_property(file):
     '''
     :param file: the xml file path to be parsed
@@ -152,10 +156,12 @@ def create_Client_node(properties):
     MERGE (cl:Client {clientName: {clientName}})
     RETURN id(cl) '''
 
-    return g.run(query, clientName = properties['clientName'])
+    return g.run(query, clientName = properties['clientName']).evaluate()
 
 
 # #========================================== Node: Issue ==========================================#
+
+
 def create_Issue_property(file):
     query = '''
     CALL apoc.load.xml({file})
@@ -163,8 +169,8 @@ def create_Issue_property(file):
     WITH dc WHERE dc._type = 'alis'
     UNWIND dc._children as alis
     UNWIND alis._children as ali_info
-    WITH collect(ali_info) as arr
-    RETURN arr
+    WITH collect(ali_info) as issue
+    RETURN issue
     '''
     pre_property = g.run(query, file = file).evaluate()
     properties = []
@@ -175,17 +181,27 @@ def create_Issue_property(file):
         if i%5 == 0:   #issueAreaCode
             dic = {}
             issueNumber += 1
+            try:
+                dic['issueAreaCode'] = property['_text']
+            except KeyError:
+                break
             dic['issueNumber'] = issueNumber
-            dic['issueAreaCode'] = property['_text']
             properties.append(dic)
 
         elif (i - 1) % 5 == 0: #des
-            dic['description'] = property['_children'][0]['_text']
+            try:
+                dic['description'] = property['_children'][0]['_text']
+            except KeyError:
+                dic['description'] = 'NULL'
 
         elif (i - 2) % 5 == 0: #fed_agen
-            dic['federal_agencies'] = property['_text']
+            try:
+                dic['federal_agencies'] = property['_text']
+            except KeyError:
+                dic['federal_agencies'] = 'NULL'
 
     return properties
+
 
 def create_Issue_node(properties):
     '''
@@ -205,241 +221,164 @@ def create_Issue_node(properties):
 
     return id_lst
 
-# #========================================== Node: Lobbyyist ==========================================#
 
-def create_Issue_property(file):
+# #========================================== Node: Lobbyist ==========================================#
+
+
+def create_Lobbyist_property(file):
     query = '''
     CALL apoc.load.xml({file})
     YIElD value UNWIND value._children as dc
     WITH dc WHERE dc._type = 'alis'
     UNWIND dc._children as alis
     UNWIND alis._children as ali_info
-    WITH collect(ali_info) as arr
-    RETURN arr
+    WITH collect(ali_info) as lobbyist
+    RETURN lobbyist
     '''
     pre_property = g.run(query, file = file).evaluate()
     properties = []
-    dic = {}
-    issueNumber = 0
+    issueNumber = 1
 
     for i, property in enumerate(pre_property):
-        if i%5 == 0:   #issueAreaCode
-            dic = {}
+
+        if (i - 3) % 5 == 0: #lobbyists
+            lobbyists = property['_children']
+            for lobbyist in lobbyists:
+                dic = {}
+                if '_text' in (lobbyist['_children'][0] and lobbyist['_children'][1]):
+                    dic['issueNumber'] = issueNumber
+                    dic['firstName'] = lobbyist['_children'][0]['_text']
+                    dic['lastName'] = lobbyist['_children'][1]['_text']
+                    try:
+                        dic['position'] = lobbyist['_children'][3]['_text']
+                    except KeyError:
+                        dic['position'] = 'NULL'
+                if dic:
+                    properties.append(dic)
             issueNumber += 1
-            dic['issueNumber'] = issueNumber
-            dic['issueAreaCode'] = property['_text']
-            properties.append(dic)
-
-        elif (i - 1) % 5 == 0: #des
-            dic['description'] = property['_children'][0]['_text']
-
-        elif (i - 2) % 5 == 0: #fed_agen
-            dic['federal_agencies'] = property['_text']
-
     return properties
 
 
-#     # Cypher returns a cursor object, each item in the cursor object represents a issuecode and lobbyists who are associated to that issuecode
-#
-#     iscd_lst = []
-#     # collect issuecodes of one disclosure
-#     lobId_collector = []
-#
-#     # find_empty = False
-#     #find_empty data
-#     for i,lobbyist_issuecode in enumerate(lobbyists_issuecode):
-#
-#         lobOrIss_item = lobbyist_issuecode['ali_info']
-#         #each item in the cursor object is a JSON object and represents a issuecode and lobbyists who are associated to that issuecode
-#         # Both lobbyists and issuecodes in the JSON object have attr '_type';
-#         # Lobbyists have attr '_children'
-#         # Issuecodes have attr '_text'
-#         if not('_text' in lobOrIss_item or '_children' in lobOrIss_item ):
-#             # The if statement check if an item doest not have attr '_text' nor '_children', the JSON object has no
-#             # issuecode data or lobbyists data to be extracted
-#             find_empty = True
-#             # here find empty data
-#             break
-#
-#         elif '_text' in lobOrIss_item:
-#             # check if '_text' in the item, the item represents an issuecode
-#             issuecode = lobOrIss_item['_text']
-#             iscd_lst.append(issuecode)
-#             # collecting issuecodes to the idscd_lst
-#             issues_node = g.run(
-#                 '''CREATE (iss:Issue {code: {code}})
-#                 RETURN id(iss)
-#                 ''', parameters = {'code': issuecode}
-#             )
-#             issue_id = [issue['id(iss)'] for issue in issues_node][0]
-#             issue_id_lst.append(issue_id)
-#             # Collect node'id of new (iss: Issue) created at each iteration
-#
-#         elif '_children' in lobOrIss_item :
-#             # check if '_children' in the item, the item represents lobbyists
-#             #lobbyists_json = lobOrIss_item
-#             lobbyists = lobOrIss_item['_children']
-#             lob_id_lst = []
-#
-#             for lobbyist in lobbyists:
-#
-#                 lobbyist_dic = {}
-#                 first_name = lobbyist['_children'][0]
-#                 last_name = lobbyist['_children'][1]
-#                 position = lobbyist['_children'][3]
-#
-#                 if ('_text' in first_name) and ('_text' in last_name):
-#
-#                     lobbyist_dic['firstName'] = first_name['_text']
-#                     lobbyist_dic['lastName'] = last_name['_text']
-#
-#                     try:
-#
-#                         lobbyist_dic['position'] = position['_text']
-#                         # position is optional in the XML file
-#
-#                     except KeyError:
-#
-#                         lobbyist_dic['position'] = "NULL"
-#
-#                     # lobbyist_lst.append(lobbyist_dic)
-#                     lobbyists_node = g.run(   # Use MERGE vs CREATE: MERGE can be problematic if 2 persons have same name and position
-#                         ''' MERGE (lob: Lobbyist {firstName: {firstName}, lastName: {lastName}, position : {position}})
-#                         RETURN id(lob)
-#                         ''', parameters={'firstName': lobbyist_dic['firstName'],
-#                                          'lastName': lobbyist_dic['lastName'],
-#                                          'position': lobbyist_dic['position']}
-#                     )
-#                     lob_id = [lobbyist['id(lob)'] for lobbyist in lobbyists_node][0]
-#                     lob_id_lst.append(lob_id)
-#                     lobId_collector.append(lob_id)
-#
-#             # print(issue_id)
-#             # print(lobbyist_lst)
-#             # print(lob_id_lst)
-#             lobbyist_iss_rel = g.run(
-#                 '''Match (iss:Issue) WHERE id(iss) = {issue_id}
-#                 MATCH (lob: Lobbyist) WHERE id(lob) in {lob_id_lst}
-#                 CREATE (lob)-[r:LOBBIES]->(iss)
-#                 ''', parameters={'issue_id': issue_id, 'lob_id_lst': lob_id_lst}
-#             )
-#
-#
-#     print(lobId_collector)
-#     # if find_empty:
-#     #     break
-# # ========================================== Node: Issue ==========================================#
-#     issuesdes = g.run(
-#         '''CALL apoc.load.xml({f})
-#         YIElD value UNWIND value._children as dl
-#         WITH dl WHERE dl._type = 'alis'
-#         UNWIND dl._children as alis
-#         UNWIND alis._children as ali_info
-#         WITH ali_info WHERE ali_info._type = 'specific_issues'
-#         UNWIND ali_info._children as specific_issues
-#         RETURN specific_issues._text''', parameters={'f': fi})
-#
-#     # desc = [des['specific_issues._text'] for des in issuesdes][0]
-#     # print(desc) "None"
-#
-#
-#     federal_agencies = g.run(
-#         '''CALL apoc.load.xml({f})
-#         YIElD value UNWIND value._children as dl
-#         WITH dl WHERE dl._type = 'alis'
-#         UNWIND dl._children as alis
-#         UNWIND alis._children as ali_info
-#         WITH ali_info WHERE ali_info._type = 'federal_agencies'
-#         RETURN ali_info._text''', parameters={'f': fi})
-#
-#     isde_lst = []
-#     feag_lst = []
-#
-#     for issuedes in issuesdes:
-#         if issuedes['specific_issues._text'] != None :
-#             isde_lst.append(issuedes['specific_issues._text'])
-#
-#     for agency in federal_agencies:
-#         if agency['ali_info._text'] != None:
-#             feag_lst.append(agency['ali_info._text'])
-#     # print(fi)
-#     # print(len(iscd_lst))
-#     # print(len(isde_lst))
-#     # print(len(feag_lst))
-#
-#     if len(iscd_lst) == len(isde_lst) and len(isde_lst) == len(feag_lst):
-#
-#         for i in range(len(iscd_lst)):
-#             issues = g.run(
-#                 ''' MATCH (iss:Issue) WHERE id(iss) in {issue_id_lst}
-#                 SET iss.des = {des}, iss.agency = {agency}
-#                 ''', parameters={'issue_id_lst': issue_id_lst, "des" : isde_lst[i], "agency": feag_lst[i]}
-#             )
-#     else:
-#         print('error')
-#
-#
-# #========================================== Rel==========================================#
-#     dc_iss_rel = g.run(
-#         '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
-#         MATCH (iss:Issue) WHERE id(iss) in {iss_id}
-#         CREATE (dc)-[r:HAS]->(iss)
-#         ''',
-#         parameters = { 'dc_id': dc_id, 'iss_id': issue_id_lst}
-#     )
-#
-#     lf_dc_rel = g.run(
-#         '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
-#         MATCH (lf:LobbyFirm) WHERE id(lf) = {lf_id}
-#         CREATE (lf)-[r:FILED]->(dc)
-#         ''',
-#         parameters={'dc_id': dc_id, 'lf_id': lf_id}
-#     )
-#
-#     lob_lf_rel = g.run(
-#         '''MATCH (lob:Lobbyist) WHERE id(lob) in {lobId_collector}
-#         MATCH (lf:LobbyFirm) WHERE id(lf) = {lf_id}
-#         MERGE (lob)-[r:WORKS_AT]->(lf)
-#         ''',
-#         parameters={'lobId_collector': lobId_collector, 'lf_id': lf_id}
-#     )
-#
-#     cl_dc_rel = g.run(
-#         '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
-#         MATCH (cl:Client) WHERE id(cl) = {cl_id}
-#         CREATE (cl)-[r:SIGNED]->(dc)
-#         ''',
-#         parameters={'dc_id': dc_id, 'cl_id': cl_id}
-#     )
+def create_lobbyist_node(properties, issueID):
+    '''
+
+    :param properties: a list of dict
+    :return: the id of issue nodes
+    '''
+
+    id_lst = []
+    query = '''
+    MERGE (lob: Lobbyist {firstName: {firstName}, lastName :{lastName}, position:{position}})
+    WITH lob MATCH (is: Issue) WHERE id(is) = {issue_id}
+    CREATE (lob)-[:LOBBIES]->(is)
+    RETURN id(lob)
+    '''
+    for i, property in enumerate(properties):
+        index = property['issueNumber']-1
+        issue_id = issueID[index]
+        id = g.run(query, firstName = property['firstName'], lastName = property['lastName'],
+                   position = property['position'], issue_id = issue_id).evaluate()
+        id_lst.append(id)
+
+    return id_lst
 
 
-pw = os.environ.get('NEO4J_PASS')
-# g= Graph("http://localhost:7474/browser/",password = pw)
-g= Graph("http://localhost:7474/",password = pw)## readme need to document setting environment variable in pycharm
-g.delete_all()
-tx = g.begin()
+
+
 
 
 #========================================== Get files ==========================================#
-root =  os.getcwd()
-path = os.path.join(root, "data")
-disclosure_1st_path = os.path.join(path, "2013_1stQuarter_XML")
-# files = [f for f in os.listdir(disclosure_1st_path) if f.endswith('.xml')]
-files = ['file:///Users/yaqi/Documents/vir_health_graph/health-graph/data/2013_1stQuarter_XML/300529246.xml'] # Return xml files
-for file in files:
-    # fi = 'file://' + os.path.join(disclosure_1st_path, file)
-    fi = file
-    print(fi)
 
-    dc_pro = get_Disclosure_property(fi)
-    dc_nodes = create_Disclousure_node(dc_pro, fi)
+if __name__ == "__main__":
+    pw = os.environ.get('NEO4J_PASS')
+    # g= Graph("http://localhost:7474/browser/",password = pw)
+    g = Graph("http://localhost:7474/", password=pw)  ## readme need to document setting environment variable in pycharm
+    g.delete_all()
+    tx = g.begin()
 
-    lf_pro = get_LobbyFirm_property(fi)
-    lf_nodes = create_LobbyFirm_node(lf_pro)
+    root =  os.getcwd()
+    path = os.path.join(root, "data")
+    disclosure_1st_path = os.path.join(path, "2013_1stQuarter_XML")
+    files = [f for f in os.listdir(disclosure_1st_path) if f.endswith('.xml')]
+    # files = ['file:///Users/yaqi/Documents/vir_health_graph/health-graph/data/2013_1stQuarter_XML/300529246.xml'] # Return xml files
+    for file in files:
+        fi = 'file://' + os.path.join(disclosure_1st_path, file)
+        # fi = file
+        print(fi)
+        dc_pro = {}
+        lf_pro = {}
+        cl_pro = {}
+        is_pro = []
+        lb_pro = []
 
-    cl_pro = get_Client_property(fi)
-    cl_nodes = create_Client_node(cl_pro)
+        dc_id = ''
+        lf_id = ''
+        cl_id = ''
+        is_id = []
+        lob_id = []
 
-    is_pro = create_Issue_property(fi)
-    is_nodes = create_Issue_node(is_pro)
-    print(is_nodes)
+        dc_pro = get_Disclosure_property(fi)
+        dc_id = create_Disclousure_node(dc_pro, fi)
+
+        lf_pro = get_LobbyFirm_property(fi)
+        lf_id = create_LobbyFirm_node(lf_pro)
+
+        cl_pro = get_Client_property(fi)
+        cl_id = create_Client_node(cl_pro)
+        # print(cl_pro)
+
+        is_pro = create_Issue_property(fi)
+        if is_pro:
+            is_id = create_Issue_node(is_pro)
+            # print(is_id)
+        # print(is_pro)
+        # print(is_id)
+            lb_pro = create_Lobbyist_property(fi)
+            # for i, ele in enumerate(lb_pro):
+                # print(i)
+                # print(ele['issueNumber']-1)
+                # print(is_id[ele['issueNumber']-1])
+                # print(lb_pro[i]['issueNumber'])
+            # print(lb_pro)
+            lob_id = create_lobbyist_node(lb_pro, is_id)
+            # print(is_id)
+        # print(lb_pro)
+
+
+    # #========================================== Rel==========================================#
+
+        lf_dc_rel = g.run(
+            '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
+            MATCH (lf:LobbyFirm) WHERE id(lf) = {lf_id}
+            CREATE (lf)-[r:FILED]->(dc)
+            ''', dc_id = dc_id, lf_id = lf_id
+        )
+
+        cl_dc_rel = g.run(
+            '''MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
+            MATCH (cl:Client) WHERE id(cl) = {cl_id}
+            CREATE (cl)-[r:SIGNED]->(dc)
+            ''',
+            dc_id= dc_id, cl_id =cl_id
+        )
+
+        if is_id:
+            dc_iss_rel = g.run(
+                ''' MATCH (dc:Disclosure) WHERE id(dc) = {dc_id}
+                MATCH (is:Issue) WHERE id(is) in {is_id}
+                CREATE (dc)-[r:HAS]->(is)
+                ''', dc_id = dc_id, is_id = is_id
+            )
+
+        if lob_id:
+            lob_lf_rel = g.run(
+                '''MATCH (lob:Lobbyist) WHERE id(lob) in {lob_id}
+                MATCH (lf:LobbyFirm) WHERE id(lf) = {lf_id}
+                MERGE (lob)-[r:WORKS_AT]->(lf)
+                ''',
+                lob_id = lob_id, lf_id = lf_id
+            )
+
+
+
+
