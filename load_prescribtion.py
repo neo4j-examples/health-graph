fddd4a6cbdbad20eacfe666d4b8ac8f4b42a9651
Yaqi Shi <yshi39@dons.usfca.edu>
2016-07-14 14:12:20 -0700

    changed LOAD csv, periotic commit

--- load_prescribtion.py
+++ load_prescribtion.py
@@ -1,85 +1,61 @@
 from py2neo import Graph, Node
+from py2neo.packages.httpstream import http
 import os
 
-def get_prescribtion_property(file, g):
+def create_prescription_node(file, g):
     query = '''
-    CALL apoc.load.csv({file},{sep:","}) yield map return * limit 5
+    USING PERIODIC COMMIT 500
+    LOAD CSV WITH HEADERS FROM {file} AS row
+    CREATE (pc:prescription {npi: row.npi, drugName: row.drug_name, genericName: row.neneric_name,
+    totalCost: toFloat(row.total_drug_cost), speciality: row.specialty_description})
+    FOREACH(ignoreME IN CASE WHEN trim(row.bene_count) <> "" THEN [1] ELSE [] END | SET pc.beneCount = row.bene_count)
+    FOREACH(ignoreME IN CASE WHEN trim(row.total_claim_count) <> "" THEN [1] ELSE [] END | SET pc.totalCalimCount = row.total_claim_count)
+    FOREACH(ignoreME IN CASE WHEN trim(row.total_drug_cost) <> "" THEN [1] ELSE [] END | SET pc.totalDrugCost = row.total_drug_cost)
+    FOREACH(ignoreME IN CASE WHEN trim(row.bene_count_ge65) <> "" THEN [1] ELSE [] END | SET pc.beneCountAge65 = row.bene_count_ge65)
+    FOREACH(ignoreME IN CASE WHEN trim(row.total_claim_count_ge65) <> "" THEN [1] ELSE [] END | SET pc.totalClaimCountAge65 = row.total_claim_count_ge65)
+    FOREACH(ignoreME IN CASE WHEN trim(row.total_drug_cost_ge65) <> "" THEN [1] ELSE [] END | SET pc.totalDrugCostAge65 = row.total_drug_cost_ge65)
     '''
 
-    return g.run(query, file = file).evaluate()
+#    index1 = '''
+#    CREATE INDEX ON: prescription(npi)
+#    '''
+#    index2 = '''
+#        CREATE INDEX ON: prescription(drugName)
+#        '''
+#    index3 = '''
+#        CREATE INDEX ON: prescription(genericName)
+#        '''
+#    g.run(index1)
+#    g.run(index2)
+#    g.run(index3)
+    return g.run(query, file = file)
 
-
-
-
-
-# Get map
-# CALL apoc.load.csv('file:///Users/yaqi/Documents/Neo4j/dev/import/PartD_Prescriber_PUF_NPI_DRUG_Aa_Al_CY2013.csv',{sep:","}) yield map return * limit 5
 
 if __name__ == "__main__":
     pw = os.environ.get('NEO4J_PASS')
     g = Graph("http://localhost:7474/", password=pw)  ## readme need to document setting environment variable in pycharm
     g.delete_all()
     tx = g.begin()
+    # http.socket_timeout = 9999
 
-    file = 'file:///Users/yaqi/Documents/Neo4j/dev/import/PartD_Prescriber_PUF_NPI_DRUG_Aa_Al_CY2013.csv'
-    print(get_prescribtion_property(file, g))
+    # file = 'file:///Users/yaqi/Documents/Neo4j/dev/import/PartD_Prescriber_PUF_NPI_DRUG_Aa_Al_CY2013.csv'
+    file = 'file:///PartD_Prescriber_PUF_NPI_DRUG_Aa_Al_CY2013.csv'
+    create_prescription_node(file, g)
 
-    # CALL
-    # apoc.load.csv('file:///Users/yaqi/Documents/Neo4j/dev/import/PartD_Prescriber_PUF_NPI_DRUG_Aa_Al_CY2013.csv',
-    #               {sep: ","})
-    # yield map
-    # WITH
-    # map,
-    # CASE
-    # WA
-    #
-    # CREATE(pc:Prescribtion
-    # {npi: map.npi, drugName: map.drug_name, genericName: map.neneric_name, totalCost: map.total_drug_cost,
-    #  speciality: map.specialty_description})
-    # WITH
-    # map, pc, CASE
-    # WHEN
-    # map.bene_count <> ''
-    # THEN
-    # SET
-    # pc.beneCount = map.bene_count
-    # WHEN
-    # map.total_claim_count
-    # IS
-    # NOT
-    # NULL
-    # THEN
-    # set
-    # pc.totalCalimCount = map.total_claim_count
-    # WHEN
-    # map.bene_count_ge65
-    # IS
-    # NOT
-    # NULL
-    # THEN
-    # set
-    # pc.beneCountAge65 = map.bene_count_ge65
-    # WHEN
-    # map.total_drug_cost_ge65
-    # IS
-    # NOT
-    # NULL
-    # THEN
-    # set
-    # pc.totalDrugCostAge65 = map.total_drug_cost_ge65
-    # WHEN
-    # map.total_claim_count_ge65
-    # IS
-    # NOT
-    # NULL
-    # THEN
-    # set
-    # pc.totalClaimCountAge65 = map.total_claim_count_ge65
-    # END
-    #
-    # load
-    # csv
-    # with headers from "" as line
-    # with line, case line.foo when '' then null else line.foo end as foo
-    # create(:User
-    # {name: line.name, foo: foo})
+
+
+    # CALL apoc.load.csv({file},{sep:","}) yield map
+    # USING PERIODIC COMMIT 500 LOAD CSV
+    # CREATE (pc:prescription {npi: map.npi, drugName: map.drug_name, genericName: map.neneric_name,
+    # totalCost: toInt(map.total_drug_cost), speciality: map.specialty_description})
+    # FOREACH(ignoreME IN CASE WHEN trim(map.bene_count) <> "" THEN [1] ELSE [] END | SET pc.beneCount = map.bene_count)
+    # FOREACH(ignoreME IN CASE WHEN trim(map.total_claim_count) <> "" THEN [1] ELSE [] END | SET pc.totalCalimCount = map.total_claim_count)
+    # FOREACH(ignoreME IN CASE WHEN trim(map.total_drug_cost) <> "" THEN [1] ELSE [] END | SET pc.totalDrugCost = map.total_drug_cost)
+    # FOREACH(ignoreME IN CASE WHEN trim(map.bene_count_ge65) <> "" THEN [1] ELSE [] END | SET pc.beneCountAge65 = map.bene_count_ge65)
+    # FOREACH(ignoreME IN CASE WHEN trim(map.total_claim_count_ge65) <> "" THEN [1] ELSE [] END | SET pc.totalClaimCountAge65 = map.total_claim_count_ge65)
+    # FOREACH(ignoreME IN CASE WHEN trim(map.total_drug_cost_ge65) <> "" THEN [1] ELSE [] END | SET pc.totalDrugCostAge65 = map.total_drug_cost_ge65)
+    # '''
+
+
+
+
