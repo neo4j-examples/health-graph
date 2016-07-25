43d7ac17efdf516167d6a38289f3042d7fcea148
Yaqi Shi <shiyaqi29@gmail.com>
2016-07-25 13:55:54 -0700

    returned 3d array with matched nodes ID

--- drug_df_rel.py
+++ drug_df_rel.py
@@ -7,6 +7,8 @@ from string_converter import sort_strings
 from string_converter import uniq_elem
 # from collections import defaultdict
 from string_converter import chop_end
+from string_converter import string_filter
+from fuzzywuzzy import fuzz
 
 if __name__ == "__main__":
     pw = os.environ.get('NEO4J_PASS')
@@ -24,15 +26,14 @@ if __name__ == "__main__":
     drug_obj = g.run(q1)
     df_obj = g.run(q2)
 
-    count = 0
-    keys = []
-    values = []
+
     drugs_lst = []
     for object in drug_obj:
         drug_dic = {}
         drug_dic['id'] = object['id(d)']
         drug_dic['labelerName'] = object['d.labelerName']
         drugs_lst.append(drug_dic)
+
 
     df_lst = []
     for object in df_obj:
@@ -41,21 +42,6 @@ if __name__ == "__main__":
         df_dic['firmName'] = object['df.firmName']
         df_lst.append(df_dic)
 
-    print(drugs_lst[:10])
-
-    # [{id: 123, firmName: aaa}, {id: 345, firmName: aaa}, {id: 456, firmName: bbb}]
-    # [{id: [123, 345], firmName : aaa}, {id:[456], firmName : bbb}]
-    # lst = [{'id': '123', 'firmName': 'aaa'}, {'id': '345', 'firmName': 'aaa'}, {'id': '456', 'firmName': 'bbb'}]
-    # d = defaultdict(list)
-    # for dict in sort_fn:
-    #     firmName = dict['firmName']
-    #     id = dict['id']
-    #     d[firmName].append(id)
-    #
-    # # print(d['bbb'])
-    # # print(d.items())
-    # print()
-
     nostring = ['inc', 'co', 'ltd', 'llc', 'pvt',
                 'spa', 'corp', 'pty', 'og', 'kg',
                 'sp', 'gp', 'lp', 'corporation', 'na',
@@ -63,7 +49,7 @@ if __name__ == "__main__":
                 'pharmaceutical', 'laboratorie', 'company', 'product', 'pharma']
 
 #lower case:
-    lc_ln = lower_case(drugs_lst[:10], 'labelerName')
+    lc_ln = lower_case(drugs_lst, 'labelerName')
     lc_fn = lower_case(df_lst, 'firmName')
 
 #remove_marks:
@@ -86,53 +72,56 @@ if __name__ == "__main__":
     uq_ln = uniq_elem(sort_ln, 'labelerName')
     uq_fn = uniq_elem(sort_fn, 'firmName')
 
-    # count = 0
-    # for ele in uq_fn:
-    #     count += 1
-    # print(count)
 
 # uq ln: 5928
 # uq_fn: 7040
 # ln: 106684
 # fn df: 6991
 
+    arr = []
+    result = []
+    num = 0
 
-#
-# print(drugs_lst[:10])
-#
+    count = 0
+    for k1 in uq_ln:
+        # count += 1
+        print(k1)
+        print(uq_ln[k1])
 
-# arr = []
-# num = 0
-# for i, names in enumerate(uq_dfp[:1000]):
-#     labeler = names
-#     result = [i, labeler]
-#
-#     match_company = []
-#     for j, df in enumerate(uq_df):
-#         company = df
-#         r1 = fuzz.partial_ratio(labeler, company)
-#         r2 = fuzz.ratio(labeler, company)
-#
-#         if r1 == 100 and r1 - r2 <= 30:
-#             # print(i, j, "r1:", r1,"r2:", r2, 'labeler'+':', labeler, ' company'+':', company)
-#             match_company.append(company)
-#
-#         elif (100 > r1 >= 95 and r2 >= 85) or (
-#                     95 > r1 >= 85 and r2 >= 90):  ### miss spell or miss a word  r1 and r2 > 95
-#             md_r1 = fuzz.partial_ratio(string_filter(labeler, nostring), string_filter(company, nostring))
-#             md_r2 = fuzz.ratio(string_filter(labeler, nostring), string_filter(company, nostring))
-#
-#             if md_r1 >= 95 and md_r2 >= 95:
-#                 match_company.append(company)
-#                 # print(i, j, "md_r1:", md_r1,"md_r2:", md_r2, 'labeler' + ':', labeler, ' company' + ':', company)
-#                 # result.append(match_company)
-#     if match_company:
-#         result.append(match_company)
-#     else:
-#         num += 1
-#     arr.append(result)
-# print(arr)
-# print(num)
+        labeler_name = k1
+        nodeId_drug = uq_ln[k1]
+        match_nodes = [nodeId_drug]
+
+        for k2 in uq_fn:
+
+            company_name = k2
+            nodeId_df = uq_fn[k2]
+            r1 = fuzz.partial_ratio(labeler_name, company_name)
+            r2 = fuzz.ratio(labeler_name, company_name)
+
+            if r1 == 100 and (r1 - r2) <= 30:
+                # print(i, j, "r1:", r1,"r2:", r2, 'labeler'+':', labeler, ' company'+':', company)
+                match_nodes.append(nodeId_df)
+
+            elif (100 > r1 >= 95 and r2 >= 85) or (95 > r1 >= 85 and r2 >= 90):  ### miss spell or miss a word  r1 and r2 > 95
+
+                md_r1 = fuzz.partial_ratio(string_filter(labeler_name, nostring), string_filter(company_name, nostring))
+                md_r2 = fuzz.ratio(string_filter(labeler_name, nostring), string_filter(company_name, nostring))
+
+                if md_r1 >= 95 and md_r2 >= 95:
+                    match_nodes.append(nodeId_df)
+                                # print(i, j, "md_r1:", md_r1,"md_r2:", md_r2, 'labeler' + ':', labeler, ' company' + ':', company)
+                                # result.append(match_company)
+        if len(match_nodes) > 1:
+            result.append(match_nodes)
+        else:
+            num += 1
+        # arr.append(result)
+
+    print(result)
+    print(num)
+
+
 
 
 
