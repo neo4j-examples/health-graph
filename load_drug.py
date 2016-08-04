46f686ae7b9ff21d0e5c0b45a94651ba00d188ba
Yaqi Shi <shiyaqi29@gmail.com>
2016-08-04 15:54:12 -0700

    add GenericDrug and rel for genericDrug, Drug

--- load_drug.py
+++ load_drug.py
@@ -1,6 +1,8 @@
 from py2neo import Graph, Node
 from load_drugfirm import create_DrugFirm_node
 import os
+from string_converter import uniq_elem
+
 
 def create_Drug_node(file):
     query = '''
@@ -12,7 +14,7 @@ def create_Drug_node(file):
         tradeName: line.PROPRIETARYNAME, labelerName: line.LABELERNAME, marketing: line.MARKETINGCATEGORYNAME, DEA: line.DEASCHEDULE, startDate:line.STARTMARKETINGDATE
         })
         RETURN id(dr), dr.labelerName
-#     '''
+        '''
 
     index = '''
     CREATE INDEX ON: Drug(labelerName)'''
@@ -23,38 +25,51 @@ def create_Drug_node(file):
 if __name__ == "__main__":
     pw = os.environ.get('NEO4J_PASS')
     g = Graph("http://localhost:7474/", password=pw)  ## readme need to document setting environment variable in pycharm
-    g.delete_all()
     tx = g.begin()
 
+    # ============================================= CREATE Drug node=============================================
     # datapath = '/Users/yaqi/Documents/data/ndctext/product.txt'
-    file = 'file:///product.txt'
+    # file = 'file:///product.txt'
+    # drugs = create_Drug_node(file)
 
-    drugs = create_Drug_node(file)
-
+    # ============================================= CREATE DrugFirm node=============================================
     # datapath = '/Users/yaqi/Documents/data/drls_reg.txt'
-    file1 = 'file:///drls_reg.txt'
+    # file1 = 'file:///drls_reg.txt'
     # load_drugfirm.g = g
-    df_node = create_DrugFirm_node(file1, g)
+    # df_node = create_DrugFirm_node(file1, g)
+
+    # ============================================= CREATE GenericDrug node=============================================
+    # ============================================= CREATE GenericDrug_drug_rel node=============================================
+    idx = '''CREATE INDEX ON :GenericDrug(genericName) '''
+    g.run(idx)
+
+    q2 = '''MATCH (d:Drug) WHERE EXISTS (d.genericName) RETURN id(d), lower(d.genericName)
+    '''
+    drugs = g.run(q2)
+
+    drugs_lst = []
+    for drug in drugs:
+        drug_dic = {}
+        drug_dic['id'] = drug['id(d)']
+        drug_dic["genericName"] = drug['lower(d.genericName)']
+        drugs_lst.append(drug_dic)
+    # print(len(drugs_lst))  #106683
+
+    uq_drug = uniq_elem(drugs_lst, 'genericName')
+    # print(len(uq_drug)) #15275
+
+    q3 = '''
+    MATCH (d:Drug) where id(d) in {drug_node}
+    WITH d
+    MERGE (gd:GenericDrug{genericName: {genericName}})
+    CREATE (d)-[:IS_GENERIC_OF]->(gd)
+    '''
+    num = 0
+    for key in uq_drug:
+        drug_node = uq_drug[key]
+        genericName = key
+        g.run(q3,drug_node=drug_node,genericName = genericName )
+        num += 1
+        print(num)
 
 
-    # for drug in drugs:
-    #     print(drug.evaluate())
-    # print(dr_node)
-
-
-
-    # LOAD
-    # CSV
-    # WITH
-    # HEADERS
-    # FROM
-    # 'file:///product.txt'
-    # AS
-    # line
-    # FIELDTERMINATOR
-    # '	'
-    # CREATE(dr:Drug
-    # {drugcode: line.PRODUCTNDC, genericName: line.NONPROPRIETARYNAME,
-    #  tradeName: line.PROPRIETARYNAME, labelerName: line.LABELERNAME, marketing: line.MARKETINGCATEGORYNAME,
-    #  DEA: line.DEASCHEDULE, startDate: line.STARTMARKETINGDATE
-    #  })
