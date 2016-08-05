8b8fd9269204286d5944bb7c65d85206357ff3b1
Yaqi Shi <shiyaqi29@gmail.com>
2016-08-05 11:25:38 -0700

    cleaned code, load df

--- load_drugfirm.py
+++ load_drugfirm.py
@@ -12,18 +12,20 @@ def create_DrugFirm_node(file, g):
     RETURN id(df), df.firmName
     '''
 
-    index = '''
-    CREATE INDEX ON: DrugFirm(firmName)'''
-
-    g.run(index)
     return g.run(query,file = file)
+
 
 if __name__ == "__main__":
     pw = os.environ.get('NEO4J_PASS')
     g = Graph("http://localhost:7474/", password=pw)  ## readme need to document setting environment variable in pycharm
-    # g.delete_all()
     tx = g.begin()
 
-    datapath = '/Users/yaqi/Documents/data/drls_reg.txt'
+    index = '''
+    CREATE INDEX ON: DrugFirm(firmName)'''
+    g.run(index)
+    print("Create index on DrugFirm(firmName)")
+
+
     file = 'file:///drls_reg.txt'
-    df_node = create_DrugFirm_node(file)
+    df_node = create_DrugFirm_node(file, g)
+    print("Finish loading DrugFirm")
